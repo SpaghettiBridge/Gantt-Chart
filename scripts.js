536e156b11927fb97bf9d956cb68a067f7b4fdb9
Chris Bates <107695083+SpaghettiBridge@users.noreply.github.com>
2022-07-20 10:29:10 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -86,7 +86,8 @@ function createGantt(data, d) {
                                 else {
                                     console.log(filteredPoint);
                                     console.log(chart.get(filteredPoint.id));
-                                    chart.get(filteredPoint.id).remove(true);
+                                    chart.get(filteredPoint.id).remove();
+                                    chart["uniqueID"].redraw();
                                 }
                                 // newData[filteredPoint[0].index] = filteredPoint[0].y
                                 // newData.push(null) //--- extra null as a workaround for bug
